cbdaabb1af8b066f2d0cd6c008f2d92d2d00cd4b
Raghavendra Bhuvan <rage28@gmail.com>
2018-09-19 09:37:14 +0530

    Remove echo

--- topic/fzf/keys.zsh
+++ topic/fzf/keys.zsh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
 
-echo "zplugin keys"
 zplugin snippet "https://github.com/junegunn/fzf/blob/master/shell/completion.zsh"
 zplugin snippet "https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh"
