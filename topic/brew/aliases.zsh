7bf4511d68a333051ad3c0d1131173b888743a50
Raghavendra Bhuvan <rage28@gmail.com>
2018-09-19 08:41:15 +0530

    Add topic brew

---
+++ topic/brew/aliases.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+exists brew && {
+  brew() {
+  case "$1" in
+    cleanup)
+      (cd "$(brew --repo)" && git prune && git gc)
+      command brew cleanup
+      command brew prune
+      rm -rf "$(brew --cache)"
+      ;;
+    bump)
+      command brew update
+      command brew upgrade
+      brew cleanup
+      ;;
+    *)
+      command brew "$@"
+      ;;
+  esac
+  }
+}
