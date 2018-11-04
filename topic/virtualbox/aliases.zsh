4b22640467e7b39fd258d960a0c083c3230f123d
Raghavendra Bhuvan <rage28@gmail.com>
2018-11-04 12:56:37 +0530

    vbox function only compatible with darwin

--- topic/virtualbox/aliases.zsh
+++ topic/virtualbox/aliases.zsh
@@ -1,21 +1,23 @@
 #!/usr/bin/env sh
 
-vbox() {
-  case "$1" in
-    unload)
-      echo "Unloading VirtualBox Kexts"
-      for id in VBoxNetAdp VBoxNetFlt VBoxUSB VBoxDrv; do
-        kextunload -b "org.virtualbox.kext.$id"
-      done
-    ;;
-    load) 
-      echo "Loading VirtualBox Kexts"
-      for id in VBoxDrv VBoxUSB VBoxNetFlt VBoxNetAdp; do
+if [ "${OS}" = "darwin" ]; then
+  vbox() {
+    case "$1" in
+      unload)
+        echo "Unloading VirtualBox Kexts"
+        for id in VBoxNetAdp VBoxNetFlt VBoxUSB VBoxDrv; do
+          kextunload -b "org.virtualbox.kext.$id"
+        done
+      ;;
+      load) 
+        echo "Loading VirtualBox Kexts"
+        for id in VBoxDrv VBoxUSB VBoxNetFlt VBoxNetAdp; do
           kextload -r "/Library/Application Support/VirtualBox" "/Library/Application Support/VirtualBox/$id.kext"
-      done
-    ;;
-    *)
-      echo "Unknown option. Only 'load' & 'unload' allowed"
-    ;;
-  esac
-}
+        done
+      ;;
+      *)
+        echo "Unknown option. Only 'load' & 'unload' allowed"
+      ;;
+    esac
+  }
+fi
