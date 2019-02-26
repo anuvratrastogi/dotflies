b8ffa497dee8e2e17dd0b59c6f4c70c4ccd056eb
Raghavendra Bhuvan <rage28@gmail.com>
2019-02-26 11:14:42 +0530

    Fix gitcerts creation

--- topic/git/configs.zsh
+++ topic/git/configs.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
 # purge existing gitcert, if any
-silrem "${HOME}/.gitcerts.pem"
+silrem "${HOME}/.gitcerts.ca.pem"
 
 # create gitcerts
-fappend "${HOME}/.gitcerts.ca.pem" "$HOME/.gitcerts.pem"
-fappend "${HOME}/.gitcerts.user.pem" "$HOME/.gitcerts.pem"
+fappend "${HOME}/.gitcerts.ca.pem" "$HOME/.gitcerts.ca.pem"
+fappend "${HOME}/.gitcerts.user.pem" "$HOME/.gitcerts.ca.pem"
